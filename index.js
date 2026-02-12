102d1fafe2fe37f588de996b1681686734609e52
jhdevgr <jhdevgr@gmail.com>
2026-02-12 10:48:39 +0100

    feat(*): change position

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import Buzon from './buzon.js';
 
 (function() {
   const persons = [
-    { name: 'Jesús', avatar: './img/pacman2.png' },
+    { name: 'Lorena', avatar: './img/pacman2.png' },
     { name: 'Jorge', avatar: './img/pacman2.png' },
     { name: 'Sauri', avatar: './img/pacman2.png' },
     { name: 'Artur', avatar: './img/pacman2.png' },
     { name: 'Javi Rodríguez', avatar: './img/pacman2.png' },
     { name: 'Marcos', avatar: './img/pacman2.png' },
+    { name: 'Jesús', avatar: './img/pacman2.png' },
   ];
 
   const $mainImage = document.querySelector('#main-image');
